fddb24ba9a682d47c0d76a4b00d4b1507f636e05
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-08-07 16:47:13 +0300

    Add PostgreSQL Helm release and namespace resource

---
+++ modules/postgres/main.tf
@@ -0,0 +1,55 @@
+
+resource "kubernetes_namespace" "postgres" {
+  metadata {
+    name = "postgres"
+  }
+}
+
+
+resource "helm_release" "postgres" {
+
+  name       = "postgres"
+  namespace  = kubernetes_namespace.postgres.metadata[0].name
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "postgresql"
+
+  version = "16.4.6"
+  values = [
+
+    yamlencode({
+
+      auth = {
+
+        postgresPassword = "postgres"
+
+        username = "admin"
+
+        password = "fuko09phsurxho"
+
+        database = "apps"
+      }
+
+
+      primary = {
+
+        persistence = {
+
+          enabled = true
+
+          size = "10Gi"
+        }
+
+
+        extendedConfiguration = <<-EOF
+          wal_level = logical
+          max_wal_senders = 10
+          max_replication_slots = 10
+        EOF
+      }
+
+    })
+  ]
+
+  wait    = true
+  timeout = 600
+}

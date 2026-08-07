c29814e8c3416d08e3a0bf70889833c69e8caa6c
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-08-07 16:43:47 +0300

    update

--- modules/debezium/main.tf
+++ modules/debezium/main.tf
@@ -14,3 +14,46 @@ resource "helm_release" "debezium_operator" {
 
   cleanup_on_fail = true
 }
+
+resource "kubernetes_manifest" "postgres_cdc" {
+
+  manifest = {
+    apiVersion = "debezium.io/v1alpha1"
+    kind       = "DebeziumServer"
+
+    metadata = {
+      name      = "postgres-cdc"
+      namespace = "debezium"
+    }
+
+    spec = {
+
+      quarkus = {
+
+        config = {
+
+          # Source database
+          "debezium.source.connector.class" = "io.debezium.connector.postgresql.PostgresConnector"
+
+          "debezium.source.database.hostname" = "postgres.default.svc.cluster.local"
+          "debezium.source.database.port"     = "5432"
+          "debezium.source.database.user"     = "debezium"
+          "debezium.source.database.password" = "password"
+          "debezium.source.database.dbname"   = "app"
+
+          "debezium.source.topic.prefix" = "postgres"
+
+
+          # Sink Kafka
+          "debezium.sink.type" = "kafka"
+
+          "debezium.sink.kafka.bootstrap.servers" = "kafka.kafka.svc.cluster.local:9092"
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.debezium_operator
+  ]
+}

6855f536d604dee3713824176062a414658bef71
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-08-08 22:47:02 +0300

    update
    Updated PostgreSQL source configuration and added recommended CDC settings.

--- modules/debezium/main.tf
+++ modules/debezium/main.tf
@@ -1,22 +1,20 @@
 
 resource "helm_release" "debezium_operator" {
-  name             = "debezium-operator"
-  repository       = "https://charts.debezium.io"
-  chart            = "debezium-operator"
-  version          = "3.2.0-final"
+  name       = "debezium-operator"
+  repository = "https://charts.debezium.io"
+  chart      = "debezium-operator"
+  version    = "3.2.0-final"
 
   namespace        = "debezium"
   create_namespace = true
 
-  wait    = true
-  timeout = 600
-  atomic  = true
-
+  wait            = true
+  timeout         = 600
+  atomic          = true
   cleanup_on_fail = true
 }
 
 resource "kubernetes_manifest" "postgres_cdc" {
-
   manifest = {
     apiVersion = "debezium.io/v1alpha1"
     kind       = "DebeziumServer"
@@ -27,27 +25,30 @@ resource "kubernetes_manifest" "postgres_cdc" {
     }
 
     spec = {
-
       quarkus = {
-
         config = {
-
-          # Source database
+          # PostgreSQL source
           "debezium.source.connector.class" = "io.debezium.connector.postgresql.PostgresConnector"
 
           "debezium.source.database.hostname" = "postgres.default.svc.cluster.local"
           "debezium.source.database.port"     = "5432"
           "debezium.source.database.user"     = "debezium"
-          "debezium.source.database.password" = "password"
+          "debezium.source.database.password" = "ebezium"
           "debezium.source.database.dbname"   = "app"
 
           "debezium.source.topic.prefix" = "postgres"
 
-
-          # Sink Kafka
+          # Kafka sink
           "debezium.sink.type" = "kafka"
-
           "debezium.sink.kafka.bootstrap.servers" = "kafka.kafka.svc.cluster.local:9092"
+
+          # Recommended PostgreSQL CDC settings
+          "debezium.source.plugin.name" = "pgoutput"
+          "debezium.source.slot.name"    = "debezium_slot"
+          "debezium.source.publication.name" = "debezium_publication"
+
+          # Initial snapshot
+          "debezium.source.snapshot.mode" = "initial"
         }
       }
     }
